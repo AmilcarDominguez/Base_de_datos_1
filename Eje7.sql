9dff8a53125790fe42a05d25a547249d307dd2f6
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-19 19:47:13 -0400

    Create Eje7.sql

---
+++ Eje7.sql
@@ -0,0 +1,11 @@
+drop table if exists agenda;
+create table agenda ( nombre varchar(20),domicilio varchar(30), telefono varchar(11));
+describe agenda;
+insert into agenda (nombre, domicilio, telefono) values ('Alberto Mores','Colon 123','4234567');
+insert into agenda (nombre,domicilio,telefono) values ( 'Juan Torres','Avellaneda 135','4458787');
+insert into agenda (nombre,domicilio,telefono) values ( 'Mariana Lopez','Urquiza 333','4545454');
+insert into agenda (nombre,domicilio,telefono) values ( 'Fernando Lopez','Urquiza 333','4545454');
+select * from agenda;
+select nombre from agenda where nombre='Juan Torres';
+select nombre,telefono,domicilio from agenda where domicilio='Colon123';
+drop table agenda;
