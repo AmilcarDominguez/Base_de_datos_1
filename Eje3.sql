460adda97d9f9e77dd8089d4e85093282b19642c
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-19 19:26:58 -0400

    Create Eje3.sql

---
+++ Eje3.sql
@@ -0,0 +1,8 @@
+create table agenda ( Nombre varchar(20), Domicilio varchar(30),Telefono varchar(11));
+
+create table agenda ( Nombre varchar(20),Domicilio varchar(30),Telefono varchar(11));
+
+show tables ;
+describe agenda ;
+drop table if exists agenda ;
+drop table agenda ;
