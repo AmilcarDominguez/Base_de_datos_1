ac38fcf3f3103227eaca63c3058d7e5d049bc0d9
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-20 13:29:29 -0400

    Update Eje15.sql

--- Eje15.sql
+++ Eje15.sql
@@ -1,3 +1,3 @@
-drop table peliculas if exists;
+drop table if exists peliculas;
 create table peliculas(codigo integer unsigned auto_increment,titulo varchar(40) not null,actor varchar(20),duracion integer unsigned, primary key (codigo) );
 describe peliculas;
