62c8559be3c587083a0579cb385c1502af2e6ccc
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-20 12:05:25 -0400

    Create Eje17.sql

---
+++ Eje17.sql
@@ -0,0 +1,10 @@
+drop table autos if exists ; 
+create table autos( patente char(6), marca varchar(20), modelo char(4), precio float unsigned, primary key(patente) );
+insert into autos (patente,marca,modelo,precio) values('ACD123','Fiat 128','1970',15000);
+insert into autos (patente,marca,modelo,precio) values('ACG234','Renault 11','1990',40000);
+insert into autos (patente,marca,modelo,precio) values('BCD333','Peugeot 505','1990',80000);
+insert into autos (patente,marca,modelo,precio) values('GCD123','Renault Clio','1990',70000);
+insert into autos (patente,marca,modelo,precio) values('BCC333','Renault Megane','1998',95000);
+insert into autos (patente,marca,modelo,precio) values('BVF543','Fiat 128','1975',20000);
+select * from autos where modelo='1990';
+select * from autos where precio>50000;
