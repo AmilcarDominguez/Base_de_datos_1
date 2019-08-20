abb01956451809a1b635664fd03601deed7e5a77
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-19 20:07:26 -0400

    Create Eje9.sql

---
+++ Eje9.sql
@@ -0,0 +1,10 @@
+create table agenda(apellido varchar(30),nombre varchar(20),domicilio varchar(30),telefono varchar(11));
+describe agenda;
+insert into agenda( apellido,nombre, domicilio,telefono ) values ( 'Mores','Alberto','Colon 123','4234567');
+insert into agenda( apellido,nombre, domicilio,telefono ) values ( 'Torres','Juan','Avellaneda 135','4458787');
+insert into agenda( apellido,nombre, domicilio,telefono ) values ( 'Lopez','Mariana','Urquiza 333','4545454');
+insert into agenda( apellido,nombre, domicilio,telefono ) values ( 'Lopez','Jose','Urquiza 333','4545454');
+insert into agenda( apellido,nombre, domicilio,telefono ) values ( 'Peralta','Susana','Gral. Paz 1234','4123456');
+set SQL_SAFE_UPDATES=0;
+delete apellido,nombre, domicilio,telefono from agenda where nombre='Juan';
+delete apellido,nombre, domicilio,telefono from agenda where telefono='4545454';
