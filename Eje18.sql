e5d6cdcdb8c573ec5c0817814fe4988723e91328
Amilcar Domínguez <54275960+AmilcarDominguez@users.noreply.github.com>
2019-08-20 13:27:59 -0400

    Update Eje18.sql

--- Eje18.sql
+++ Eje18.sql
@@ -1,3 +1,3 @@
-create table pedidos( nro_pedido; nombre; tipo; precio; cantidad; domicilio; );
+create table pedidos( nro_pedido, nombre, tipo, precio, cantidad, domicilio);
 drop table if exists pedidos;
 create table pedidos( nro_pedido tinyint unsigned auto_incremet; nombre varchar(8); tipo varchar(20); precio decimal(2,1); cantidad tinyint unsigned; domicilio varchar(50); primary key (nro_pedido) );
